6946d9ba47e593a6e4d3027c233ac159dee9cb29
BastienLaffon <bastien.laffon.pro@gmail.com>
2021-11-23 09:47:15 +0100

    second push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FormComponent} from "./form/form.component";
+import {HomeComponent} from "./home/home.component";
 
 const route: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'formulaire', component: FormComponent },
+  { path: 'home', component: HomeComponent },
+
+
 ];
 
 @NgModule({
@@ -11,3 +16,4 @@ const route: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FormComponent,HomeComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import {RouterModule} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    routingComponents,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
