fb16a8e46cbfd57d2e11c961036608232eb099e8
BastienLaffon <bastien.laffon.pro@gmail.com>
2021-11-23 09:05:42 +0100

    init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {FormComponent} from "./form/form.component";
+
+const route: Routes = [
+  { path: 'formulaire', component: FormComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(route)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
